c1fbd82ae7d8bc3b0d740ca027acdb1496b9f9e2
Brandon Rose <rose.brandon.m@gmail.com>
2021-03-01 14:15:36 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dummy Model
+
+This repo is for model testing. It provides a test dummy model.
+
+## Usage
+
+Install with `pip -r requirements.txt`.
+
+Next, edit the rainfall setting in `parameters.json` to a value between 1 and 100.
+
+Then run with `python3 main.py output.csv`.
+
+You can replace `output.csv` in the above command with the name of a CSV file you wish to output.
---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import random
+import json
+import pandas as pd
+from faker import Faker
+from collections import OrderedDict
+
+if __name__ == "__main__":
+    outfile = sys.argv[1]
+    params = open('parameters.json').read()
+    params = json.loads(params)
+
+    locales = OrderedDict([
+        ('en-US', 1)
+    ])
+    fake = Faker(locales)
+    f = fake['en_US']    
+    data = []
+
+    for i in range(100):
+        obj = dict(latitude=f.latitude(),
+                   longitude=f.longitude(),
+                   date=f.date(),
+                   value=random.random()*params['rainfall'])
+        data.append(obj)
+
+    df = pd.DataFrame(data)
+    print(df.head())
+    df.to_csv(outfile,index=False)
